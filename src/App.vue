456d5bbcc209973b064bc91e734571ae95cc4e48
mxz96102 <mxz96102@qq.com>
2017-04-08 19:21:49 +0800

    4-20:52

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,6 @@
       <div v-if="$route.path!='/'&&$route.path!='/center'&&$route.path!='/complete'" class="back-button" >
           <a @Click="()=>history.back()"><svg version="1.1" class="back-button" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 	 viewBox="0 0 25 25" style="enable-background:new 0 0 25 25;" xml:space="preserve">
-        <style type="text/css">
-	        .st0{fill:#7A8783;stroke:#00FFB8;stroke-miterlimit:10;}
-             .st1{fill:none;stroke:#00FFB8;stroke-miterlimit:10;}
-        </style>
         <circle id="XMLID_1_" class="st0" cx="12.5" cy="12.5" r="11"/>
         <polyline id="XMLID_12_" class="st1" points="8.5,12.6 15.5,6.7 8.5,12.6 15.5,18.4 "/>
       </svg>
@@ -59,4 +55,18 @@ html,body{
 div{
   box-sizing: border-box;
 }
+.back-button{
+    width:40px;
+    height: 40px;
+    filter:alpha(opacity=75);
+    -moz-opacity:0.75;
+    opacity: 0.75;
+    position: fixed;
+    bottom: 20px;
+    left: 20px;
+    text-align: center;
+    z-index: 10;
+}
+.st0{fill:#7A8783;stroke:#00FFB8;stroke-miterlimit:10;}
+.st1{fill:none;stroke:#00FFB8;stroke-miterlimit:10;}
 </style>
